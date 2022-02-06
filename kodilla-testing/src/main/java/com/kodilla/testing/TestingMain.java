890ceb6c39de3f09c35e74b1048ab1888a8304c2
MrLemonCoffee <grzkry1@wp.pl>
2022-02-06 20:28:49 +0100

    Module 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,40 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        System.out.println("\n -------Test 6.2------- \n");
+                Calculator calculator = new Calculator ();
+
+                 int addResult = calculator.add(1,2);
+
+                 int substractResult = calculator.subtract(1,2);
+                //test1
+                if (addResult==3){
+                    System.out.println("Test 6.2.1 addResult is OK");
+                } else {
+                    System.out.println("Test 6.2.1 addResult ERROR");
+                }
+                //test2
+                if (substractResult==-1){
+                    System.out.print("Test 6.2.2 substractResult is OK");
+                } else {
+                    System.out.print("Test 6.2.2 substractResult ERROR");
+                }
+
+
+
+
     }
 }
