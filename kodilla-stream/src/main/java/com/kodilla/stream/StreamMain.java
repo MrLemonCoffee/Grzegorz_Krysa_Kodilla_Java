97d51e0154818367915a33d9542285e3bef176de
MrLemonCoffee <grzkry1@wp.pl>
2022-03-16 18:24:23 +0100

    Module 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,27 +1,19 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.lambda.*;
-import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
+
     public static void main(String[] args) {
-
-        Processor processor = new Processor();
-        processor.execute(() -> System.out.println("This is an example text!"));
-
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-        expressionExecutor.executeExpression(10, 5, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(10, 5, FunctionalCalculator:: subBFromA);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-
         PoemBeautifier poemBeautifier = new PoemBeautifier();
-        poemBeautifier.beautify("orange", String::toUpperCase);
-        poemBeautifier.beautify("ORANGE", (text) -> text.toLowerCase());
-        poemBeautifier.beautify("orange", (text) -> "big " + text);
-        poemBeautifier.beautify("orange", (text) -> text.substring(0, 4));
-        poemBeautifier.beautify("orange", (text) -> text.replace("a","A"));
+
+        poemBeautifier.beautify("Beautiful and interesting text",text -> text.toUpperCase());
+        poemBeautifier.beautify("Beautiful and interesting text",text -> "ABC "+text  );
+        poemBeautifier.beautify("Beautiful and interesting text",text -> text + " ABC");
+        poemBeautifier.beautify("Beautiful and interesting text",text -> text.substring(0,9));
+        poemBeautifier.beautify("Beautiful and interesting text",text -> text.replace("e","B"));
+        poemBeautifier.beautify("Beautiful and interesting text",text -> text.replace("and interesting","and touching"));
 
     }
+
 }
