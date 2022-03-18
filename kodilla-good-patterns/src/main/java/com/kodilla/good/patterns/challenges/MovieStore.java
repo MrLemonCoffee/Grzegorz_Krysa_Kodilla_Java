a1a02e6dd0128c1d15afba36dc3719b006fdbeb2
MrLemonCoffee <grzkry1@wp.pl>
2022-03-18 17:28:00 +0100

    Module 9.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class MovieStore {
+
+    public Map<String, List<String>> getMovies() {
+        List<String> ironManTranslations = new ArrayList<>();
+        ironManTranslations.add("Żelazny Człowiek");
+        ironManTranslations.add("Iron Man");
+
+        List<String> avengersTranslations = new ArrayList<>();
+        avengersTranslations.add("Mściciele");
+        avengersTranslations.add("Avengers");
+
+        List<String> flashTranslations = new ArrayList<>();
+        flashTranslations.add("Błyskawica");
+        flashTranslations.add("Flash");
+
+        Map<String, List<String>> booksTitlesWithTranslations = new HashMap<>();
+        booksTitlesWithTranslations.put("IM", ironManTranslations);
+        booksTitlesWithTranslations.put("AV", avengersTranslations);
+        booksTitlesWithTranslations.put("FL", flashTranslations);
+
+        return booksTitlesWithTranslations;
+    }
+    public String showMovieTitles() {
+        Map<String, List<String>> titles = getMovies();
+        String movieTitles = titles.values().stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.joining("! ", "", ""));
+        return movieTitles;
+    }
+
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+        System.out.println(movieStore.showMovieTitles());
+    }
+}
