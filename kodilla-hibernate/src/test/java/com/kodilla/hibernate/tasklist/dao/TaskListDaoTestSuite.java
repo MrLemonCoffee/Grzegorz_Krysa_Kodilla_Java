282842d264060cab000fdf9cf169e52dbd90027c
MrLemonCoffee <grzkry1@wp.pl>
2022-04-03 22:42:59 +0200

    Module 13.2

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,37 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class TaskListDaoTestSuite {
+
+
+    @Autowired
+    private TaskListDao taskListDao;
+    private static final String DESCRIPTION = "Test: Learn Hibernate";
+
+    @Test
+    void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList("Name", DESCRIPTION);
+        taskListDao.save(taskList);
+        String listName = taskList.getListName();
+
+        //When
+        List<TaskList> readTasksList = taskListDao.findByListName(listName);
+
+        //Then
+        assertEquals(1, readTasksList.size());
+
+        //CleanUp
+        taskListDao.delete(taskList);
+
+    }
+}
